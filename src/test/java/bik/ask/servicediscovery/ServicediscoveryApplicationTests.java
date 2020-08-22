e29e1f923276892d5ac69502ec6686d8d04563d2
bikask <bikask>
2020-08-22 18:38:14 +0530

    pshing to a diff branch

---
+++ src/test/java/bik/ask/servicediscovery/ServicediscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package bik.ask.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicediscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
